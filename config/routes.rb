6801e5928780ddf75d83da27fd0b261f6424ff43
Chas Parsons <chasparsons@gmail.com>
2019-03-10 18:40:18 -0400

    fixed a bug in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   
   devise_for :users
   root to: 'surgeons#index'
   
   resources :cpts
-=======
   resources :cpt_codes
   resources :opcodes
   resources :complications
->>>>>>> d7457d94249adc4805dbc5ca27b6304c398cb1ac
   resources :states
   resources :operations
   resources :patients
