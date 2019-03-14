65bb51c2e441e53083b7d47b8754fec60181cdeb
Chas Parsons <chasparsons@gmail.com>
2019-03-13 23:51:49 -0400

    changing the operations to add cpt

--- app/models/cpt.rb
+++ app/models/cpt.rb
@@ -1,5 +1,5 @@
 class Cpt < ApplicationRecord
-	has_many :cpt_operations,dependent: :destroy
+	has_many :cpt_operations, dependent: :destroy
 	has_many :operations, :through => :cpt_operations
 
 	scope :category, -> (category) { where category: category}
